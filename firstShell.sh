f2c3670a13c6e75b55181d518fca4066dfe71d7e
Ivan Chan <ichan018@ucr.edu>
2020-06-07 21:53:58 -0700

    Learn variables better

--- firstShell.sh
+++ firstShell.sh
@@ -1,19 +1,12 @@
 
-echo "Hello World!"
+#echo "Hello World!"
 
 #title = "Ivan's first shell script"
 
-userInfo="I love shell scripts "
+theTime="$(date +"%x %R %Z")"
+
+userInfo="the era of $theTime"
 
 cat <<- _EOF_
- <html>
-<head>
-  <title>
   Adventures in $userInfo
-  </title>
-</head>
-<body>
-   <h2>Ivan's life</h2>
-</body>
-</html>
 _EOF_
