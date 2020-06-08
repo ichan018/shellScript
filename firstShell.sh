e85756d8d426800111ddb86f25bd43cb1a0ec275
Ivan Chan <ichan018@ucr.edu>
2020-06-07 21:43:27 -0700

    Learn variables

--- firstShell.sh
+++ firstShell.sh
@@ -1,2 +1,19 @@
 
 echo "Hello World!"
+
+#title = "Ivan's first shell script"
+
+userInfo="I love shell scripts "
+
+cat <<- _EOF_
+ <html>
+<head>
+  <title>
+  Adventures in $userInfo
+  </title>
+</head>
+<body>
+   <h2>Ivan's life</h2>
+</body>
+</html>
+_EOF_
